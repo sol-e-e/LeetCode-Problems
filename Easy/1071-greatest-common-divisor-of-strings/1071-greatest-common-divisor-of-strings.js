9e04be042c80d57d15ddeab778a939c69da4c72c
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2023-07-04 17:44:37 +0900

    Time: 68 ms (35.10%), Space: 41.6 MB (97.59%) - LeetHub

--- Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -3,16 +3,8 @@
  * @param {string} str2
  * @return {string}
  */
-var gcdOfStrings = function(str1, str2) {
-    let result = "";
-    const [len1, len2] = [str1.length, str2.length];
-    let len = Math.min(len1, len2);
-    for (let i = len; i > 0; i--) {
-        if (len1 % i !== 0 || len2 % i !== 0) continue;
-        const [n1, n2] = [len1 / i, len2 / i];
-        const base = str1.slice(0, i);
-        if (str1 === base.repeat(n1) && str2 === base.repeat(n2)) return base;
-       
-    }
-    return "";
+const gcdOfStrings = function(str1, str2) {
+    if (str1 + str2 !== str2 + str1) return "";
+    const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+    return str1.substring(0, gcd(str1.length, str2.length));
 };
