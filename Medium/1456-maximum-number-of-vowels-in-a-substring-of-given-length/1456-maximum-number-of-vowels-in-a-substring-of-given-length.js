609f309af3d6bd60139f3b6742ed6a3b30045239
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2024-09-04 11:44:52 +0900

    Time: 70 ms (72.99%), Space: 52.9 MB (48.53%) - LeetHub

--- Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -5,11 +5,11 @@
  */
 var maxVowels = function(s, k) {
     let result = 0;
-    const vowel = ['a', 'e', 'i', 'o', 'u'];
+    const vowel = new Set(['a', 'e', 'i', 'o', 'u']);
     let count = 0;
     
     for (let i = 0; i < k; i++) {
-        if (vowel.includes(s[i])) {
+        if (vowel.has(s[i])) {
             count++;
         }
     }
@@ -17,8 +17,8 @@ var maxVowels = function(s, k) {
     result = count;
     
     for (let i = k; i < s.length; i++) {
-        if (vowel.includes(s[i - k])) count--;
-        if (vowel.includes(s[i])) count++;
+        if (vowel.has(s[i - k])) count--;
+        if (vowel.has(s[i])) count++;
         result = Math.max(result, count);
     }
     
