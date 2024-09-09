eef2b558c5bd7f27a166a05932d008d6dbfea5df
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2024-09-09 15:51:41 +0900

    Time: 70 ms (28.47%), Space: 54 MB (63.72%) - LeetHub

--- Medium/1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ Medium/1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -4,25 +4,20 @@
  * @return {number}
  */
 var longestOnes = function(nums, k) {
+    let flipped = 0;
+    let start = 0;
     let max = 0;
-    let count = 0;
-    const flips = [];
-    
+  
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 1) {
-            count++;
-            continue;
+        if (nums[i] === 0) flipped++;
+        
+        if (flipped > k) {
+            if (nums[start] === 0) flipped--;
+            start++;
         }
         
-        flips.push(i);
-        
-        if (flips.length <= k) {
-            count++;
-        } else {
-            max = count > max ? count : max;
-            count = i - flips.shift();
-        }
+        max = Math.max(max, i - start + 1);
     }
     
-    return count > max ? count : max;
+    return max;
 };
