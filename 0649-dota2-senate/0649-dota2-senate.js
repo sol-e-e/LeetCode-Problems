7a163a62c88cb45fb877591fede1fb8818d24da3
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2024-10-04 13:16:06 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} senate
+ * @return {string}
+ */
+var predictPartyVictory = function(senate) {
+    const queue = senate.split('');
+    let rl = 0;
+    let dl = 0;
+
+ // DRRDRDRDRDDRDRDR
+ // DRRDRDRDRDDRDRDR
+  // DRRDRDRDRDDRDRDR
+    while (queue.length > 1) {
+
+    }
+};
