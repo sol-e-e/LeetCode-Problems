def111ce6a66d35a512608f5b1416047fda5fc08
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2024-10-28 15:29:41 +0900

    Time: 38 ms (7.14%), Space: 69.1 MB (51.61%) - LeetHub

--- Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.ts
+++ Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.ts
@@ -13,29 +13,29 @@
  */
 
 function maxLevelSum(root: TreeNode | null): number {
-    let answer = 1;
-    let max = root.val;
-    let level = 1;
+    let maxLevel = 1;
+    let maxSum = root.val;
+    let curLevel = 1;
     const queue = [];
 
     queue.push(root);
 
     while(queue.length) {
-        let levelSize = queue.length;
-        let sum = 0;
-        while(levelSize) {
-            let cur = queue.shift();
-            sum += cur.val;
+        let len = queue.length;
+        let levelSum = 0;
+        for(let i = 0; i < len; ++i) {
+            const cur = queue.shift();
+            levelSum += cur.val;
+
             if (cur.left) queue.push(cur.left);
             if (cur.right) queue.push(cur.right);
-            levelSize--;
         }
-        if (sum > max) {
-            answer = level;
-            max = sum;
+        if (levelSum > maxSum) {
+            maxLevel = curLevel;
+            maxSum = levelSum;
         }
-        level++;
+        curLevel++;
     }
 
-    return answer;
+    return maxLevel;
 };
