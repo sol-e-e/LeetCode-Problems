8e9dc79826c05e61df07d9343102b4f6331760f2
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2024-11-11 12:17:24 +0900

    Time: 56 ms (38.67%), Space: 60.5 MB (63.05%) - LeetHub

--- Medium/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.ts
+++ Medium/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.ts
@@ -1,21 +1,19 @@
 function nearestExit(maze: string[][], entrance: number[]): number {
     const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
     let steps = Infinity;
-    const visited = new Set<string>();
     const cells = [[...entrance, 0]];
 
     while (cells.length) {
         const [cx, cy, cd] = cells.shift();
-        if (visited.has(cx + ',' + cy)) continue;
-
-        visited.add(cx + ',' + cy);
+        if (maze[cx][cy] === '+') continue;
+        maze[cx][cy] = '+';
 
         for (const [x, y] of directions) {
             const nx = cx + x;
             const ny = cy + y;
 
             if (nx < 0 || ny < 0 || nx >= maze.length || ny >= maze[0].length) continue;
-            if (maze[nx][ny] === '+' || visited.has(nx + ',' + ny)) continue;
+            if (maze[nx][ny] === '+') continue;
 
             if (nx === 0 || ny === 0 || nx === maze.length - 1 || ny === maze[0].length - 1) {
                 steps = Math.min(steps, cd + 1);
