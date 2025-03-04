1abc8ba75d98328bdddab55c57bccc0e6b6cde61
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-03-04 13:04:48 +0900

    Time: 48 ms (88.69%), Space: 57 MB (5.76%) - LeetHub

---
+++ Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.ts
@@ -0,0 +1,10 @@
+function mergeAlternately(word1: string, word2: string): string {
+    let result = '';
+
+    for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+        result += (word1[i] || '');
+        result += (word2[i] || '');
+    }
+
+    return result;
+};
