35f64857301f608612f2c4c4f3e01639efc5ad01
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-03-18 15:07:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ Medium/0134-gas-station/0134-gas-station.ts
@@ -0,0 +1,20 @@
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+    if (gas.reduce((a, c, i) => a + c - cost[i], 0) < 0) return -1;
+    
+    let start = 0;
+    const len = gas.length;
+    gas = gas.map((g, i) => g - cost[i]);
+    let tank = gas[0];
+
+    for (let i = 0; i < len * 2; i++) {
+        const cur = i % len;
+        if (gas[cur] > gas[cur] + tank) {
+            tank = gas[cur];
+            start = cur;
+        } else {
+            tank += gas[cur];
+        }
+    }
+
+    return start;
+};
