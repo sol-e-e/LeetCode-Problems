06a32cfc991bb15cf02ebdbaeb15f228020e0a13
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-03-21 11:25:29 +0900

    Time: 12 ms (70.84%), Space: 62 MB (10.05%) - LeetHub

--- Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.ts
+++ Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.ts
@@ -1,15 +1,17 @@
+type OperatorsMap = Map<string, (a: number, b: number) => number>
 function evalRPN(tokens: string[]): number {
-    const operators: { [key: string]: (a: number, b: number) => number } = {
-        '+': (a: number, b: number) => a + b,
-        '-': (a: number, b: number) => a - b,
-        '*': (a: number, b: number) => a * b,
-        '/': (a: number, b: number) => a / b | 0,
-    }
+    const operators: OperatorsMap = new Map([
+        ['+', (a: number, b: number) => a + b],
+        ['-', (a: number, b: number) => a - b],
+        ['*', (a: number, b: number) => a * b],
+        ['/', (a: number, b: number) => a / b | 0]
+    ]);
+
     const nums: number[] = [];
     for (const token of tokens) {
-        if (operators[token] !== undefined) {
+        if (operators.has(token)) {
             const [rear, front] = [nums.pop(), nums.pop()];
-            nums.push(operators[token](front, rear));
+            nums.push(operators.get(token)(front, rear));
         } else {
             nums.push(Number(token));
         }
