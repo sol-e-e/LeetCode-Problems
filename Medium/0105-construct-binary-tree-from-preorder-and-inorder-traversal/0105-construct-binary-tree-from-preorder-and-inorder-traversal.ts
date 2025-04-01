03067519b6c99340617f417dffddec55368b753b
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-04-01 13:29:27 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+    if (!preorder.length || !inorder.length) return null;
+
+    const current = preorder[0];
+    const node = new TreeNode(current);
+    const index = inorder.findIndex(i => i === current);
+
+    node.left = buildTree(preorder.slice(1), inorder.slice(0, index));
+    node.right = buildTree(preorder.slice(node.left ? 2 : 1), inorder.slice(index + 1));
+
+    return node;
+};
