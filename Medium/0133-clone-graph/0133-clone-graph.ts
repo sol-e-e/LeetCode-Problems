14135b167a8c45d876a4e230c56d3848cad6d818
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-04-07 15:35:00 +0900

    Time: 60 ms (37.65%), Space: 56.9 MB (70.44%) - LeetHub

---
+++ Medium/0133-clone-graph/0133-clone-graph.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     neighbors: _Node[]
+ * 
+ *     constructor(val?: number, neighbors?: _Node[]) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.neighbors = (neighbors===undefined ? [] : neighbors)
+ *     }
+ * }
+ * 
+ */
+
+
+function cloneGraph(node: _Node | null): _Node | null {
+    const visited = new Map<_Node, _Node>();
+    return clone(node, visited);
+};
+
+function clone(node: _Node | null, visited: Map<_Node, _Node>): _Node | null {
+    if (!node) return node;
+
+    if (visited.has(node)) {
+        return visited.get(node);
+    }
+
+    const copy = new _Node(node.val);
+    visited.set(node, copy);
+
+    for (const neighbor of node.neighbors) {
+        copy.neighbors.push(clone(neighbor, visited));
+    }
+
+    return copy;
+}
