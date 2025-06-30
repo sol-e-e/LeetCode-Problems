5a4b56b59bbd71db029b49284d5e28c4ed73bb2a
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-06-30 11:34:54 +0900

    Time: 0 ms (100%), Space: 55.8 MB (27.61%) - LeetHub

---
+++ Easy/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    const answer: number[] = [];
+    const stack: TreeNode[] = [];
+    let current = root;
+
+    while (current || !!stack.length) {
+        while (current) {
+            stack.push(current);
+            current = current.left;
+        }
+
+        current = stack.pop();
+        answer.push(current.val);
+        current = current.right;
+    }
+
+    return answer;
+};
