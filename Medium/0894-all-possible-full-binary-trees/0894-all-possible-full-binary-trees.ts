112516b98cecd70596c758163176920e9529d29f
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-07-17 14:14:50 +0900

    Time: 10 ms (15.38%), Space: 70.6 MB (23.08%) - LeetHub

---
+++ Medium/0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.ts
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+
+function allPossibleFBT(n: number): Array<TreeNode | null> {
+    const result: Array<TreeNode | null> = [];
+
+    if (n % 2 === 0) return result;
+
+    if (n === 1) {
+        result.push(new TreeNode(0));
+        return result;
+    }
+
+    n -= 1;
+
+    for (let i = 0; i < n; i++) {
+        const left: Array<TreeNode | null> = allPossibleFBT(i);
+        const right: Array<TreeNode | null> = allPossibleFBT(n - i);
+
+        for (let l of left) {
+            for (let r of right) {
+                const current = new TreeNode(0);
+                current.left = l;
+                current.right = r;
+
+                result.push(current);
+            }
+        }
+    }
+
+    return result;
+};
