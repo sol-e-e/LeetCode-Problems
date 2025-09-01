8a90af72bed3084eaba8afee9822c9115226e91e
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-09-01 17:04:33 +0900

    Time: 724 ms (14.29%), Space: 93.5 MB (85.71%) - LeetHub

--- Medium/1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.ts
+++ Medium/1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.ts
@@ -1,92 +1,87 @@
 function maxAverageRatio(classes: number[][], extraStudents: number): number {
-    const heap = new Heap();
+    const ratioIncrement = (pass: number, total: number): number => {
+        return (pass + 1) / (total + 1) - pass / total;
+    }
+
+    const maxHeap = new MaxHeap();
     for (let [pass, total] of classes) {
-        heap.insert([pass, total]);
+        maxHeap.insert([ratioIncrement(pass, total), pass, total]);
     }
 
     for (let i = 0; i < extraStudents; i++) {
-        const {pass, total} = heap.extract();
-        heap.insert([pass + 1, total + 1]);
+        const [_, pass, total] = maxHeap.extract()!;
+         maxHeap.insert([ratioIncrement(pass + 1, total + 1), pass + 1, total + 1]);
     }
 
-    return heap.getRatio();
+    let totalRatio = 0;
+    while (maxHeap.heap.length > 0) {
+        const [_, pass, total] = maxHeap.extract()!;
+        totalRatio += pass / total;
+    }
+
+    return totalRatio / classes.length;
 };
 
-type HeapType = { pass: number, total: number, ratio: number, change: number };
-
-class Heap {
-    private heap: HeapType[]
-    constructor() {
-        this.heap = [];
-    }
-
-    compare(parent: HeapType, child: HeapType) {
-        return parent.change - child.change;
-    }
-
-    swap(index1, index2) {
-        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
-    }
-
-    insert(value: number[]) {
-        const [pass, total] = value;
-        this.heap.push({pass, total, ratio: pass / total, change: (pass + 1) / (total + 1) - pass / total});
+class MaxHeap {
+    heap: [number, number, number][] = []; // ratio increment, pass, total
+    public insert(item: [number, number, number]) {
+        this.heap.push(item);
         this.heapifyUp();
     }
 
-    extract(): HeapType | null {
-        if (this.heap.length === 0) return null;
-        
+    public extract(): [number, number, number] | undefined {
+        if (this.heap.length === 0) return undefined;
+
         const max = this.heap[0];
         const last = this.heap.pop();
-        
+
         if (this.heap.length > 0) {
-            this.heap[0] = last!;
+            this.heap[0] = last;
             this.heapifyDown();
         }
-        
+
         return max;
     }
 
-    heapifyUp() {
+    private heapifyUp() {
         let index = this.heap.length - 1;
+        const el = this.heap[index];
+
         while (index > 0) {
             const parentIndex = Math.floor((index - 1) / 2);
-             if (this.compare(this.heap[parentIndex], this.heap[index]) < 0) {
-                this.swap(parentIndex, index);
-                index = parentIndex;
-            } else {
-                break;
-            }
+            const parent = this.heap[parentIndex];
+
+            if (el[0] <= parent[0]) break;
+
+            this.heap[index] = parent;
+            index = parentIndex;
         }
+
+        this.heap[index] = el;
     }
 
-    heapifyDown() {
+    private heapifyDown() {
         let index = 0;
         const length = this.heap.length;
+
         while (true) {
-            const leftChildIndex = index * 2 + 1;
-            const rightChildIndex = index * 2 + 2;
-            let largest = index;
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let targetIndex = index;
 
             if (leftChildIndex < length && 
-            this.compare(this.heap[largest], this.heap[leftChildIndex]) < 0) {
-                largest = leftChildIndex;
+            this.heap[leftChildIndex][0] > this.heap[targetIndex][0]) {
+                targetIndex = leftChildIndex;
             }
 
-            if (rightChildIndex < length && 
-            this.compare(this.heap[largest], this.heap[rightChildIndex]) < 0) {
-                largest = rightChildIndex;
+             if (rightChildIndex < length && 
+            this.heap[rightChildIndex][0] > this.heap[targetIndex][0]) {
+                targetIndex = rightChildIndex;
             }
 
-            if (largest === index) break;
-            
-            this.swap(index, largest);
-            index = largest;
+            if (targetIndex === index) break;
+            [this.heap[targetIndex], this.heap[index]] = [this.heap[index], this.heap[targetIndex]];
+            index = targetIndex;
         }
-    }
-
-    getRatio() {
-        return this.heap.reduce((acc, {ratio}) => acc + ratio, 0) / this.heap.length;
     }
 }
