74d697c0d46449e47916a75a4b8046e40d1c1fcb
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-10-20 17:40:11 +0900

    Time: 894 ms (100%), Space: 72.4 MB (100%) - LeetHub

--- Medium/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.ts
+++ Medium/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.ts
@@ -1,47 +1,52 @@
 function maxTargetNodes(edges1: number[][], edges2: number[][], k: number): number[] {
-    const max = Math.max(...countNodes(makeTree(edges2), k - 1));
+    const n = edges1.length + 1, m = edges2.length + 1;
+    const adj1 = Array.from({ length: n }, () => []);
+    const adj2 = Array.from({ length: m }, () => []);
 
-    return countNodes(makeTree(edges1), k).map((v) => v + max);
+    for (const [u, v] of edges1) {
+        adj1[u].push(v);
+        adj1[v].push(u);
+    }
+
+    for (const [u, v] of edges2) {
+        adj2[u].push(v);
+        adj2[v].push(u);
+    }
+
+    const path = Array(n).fill(0).map((_, i) => bfs(i, adj1, k));
+
+    let max = 0;
+    if (k > 0) {
+        for (let i = 0; i < m; i++) {
+            max = Math.max(max, bfs(i, adj2, k - 1));
+        }
+    }
+
+    return path.map(p => p + max);
 };
 
-function makeTree(edges: number[][]): Record<string, number[]> {
-    let tree: Record<string, number[]> = {}; 
-    for (const [u, v] of edges) {
-        if (!tree[u]) tree[u] = [];
-        if (!tree[v]) tree[v] = [];
-        tree[u].push(v);
-        tree[v].push(u);
-    }
-    return tree;
-}
+function bfs(start: number, adj: number[][], k: number) {
+    if (k === 0) return 1;
+    const visited = new Set();
+    const queue = [start];
+    visited.add(start);
+    let h = k;
+    let nodes = 1;
 
-function countNodes(tree: Record<string, number[]>, height: number): number[] {
-    let result: number[] = [];
-
-    for (const [key, value] of Object.entries(tree)) {
-        let queue = [];
-        let visited = new Set<number>();
-        let count = 0, h = height;
-        queue.push(Number(key));
-        while (queue.length && h >= 0) {
-            const next = [];
-        
-            for (const node of queue) {
-                visited.add(node);
-                count += 1;
-                for (const nextNode of tree[node]) {
-                    if (!visited.has(nextNode)) {
-                        next.push(nextNode);
-                    }
+    while (queue.length && h > 0) {
+        const size = queue.length;
+        for (let i = 0; i < size; i++) {
+            const node = queue.shift();
+            for (const neighbor of adj[node]) {
+                if (!visited.has(neighbor)) {
+                    visited.add(neighbor);
+                    queue.push(neighbor);
+                    nodes += 1;
                 }
-            }
-
-            queue = [...next];
-            h--;
+            } 
         }
-
-        result.push(count);
+        h -= 1;
     }
 
-    return result;
+    return nodes;
 }
