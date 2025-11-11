9492eda4f5c1eb187a6369a29d0c48eddc5da7f3
sol-e-e <105342384+sol-e-e@users.noreply.github.com>
2025-11-11 14:28:19 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- Easy/2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.ts
+++ Easy/2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.ts
@@ -2,10 +2,9 @@ function divideString(s: string, k: number, fill: string): string[] {
     const len = s.length;
     const groups = Math.ceil(len / k);
     const result = [];
-        console.log(s)
 
-    for (let i = 0; i < groups; i++) {
-        const group = s.substring(i * k, i * k + k).padEnd(k, fill);
+    for (let i = 0; i < groups; i += k) {
+        const group = s.substring(i, i + k).padEnd(k, fill);
         result.push(group);
     }
     
